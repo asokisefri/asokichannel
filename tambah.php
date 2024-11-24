c21d7aaecefc5ff27aa880aa0c1c21b96aabed4c
asokisefri <asokisefrilamba03@gmail.com>
2024-11-24 15:30:15 +0700

    Add files via upload

---
+++ tambah.php
@@ -0,0 +1,31 @@
+<html>
+
+<form method="post" action="tambahaksi.php">
+
+<h2> INPUT DATA </h2>
+
+<table>
+<tr>
+<td> Nama  </td>
+<td> <input type="text" name="nama">
+</tr>
+
+<tr>
+<td> Nim  </td>
+<td> <input type="text" name="nim">
+</tr>
+
+
+<tr>
+<td></td>
+<td> <input type="submit" name="Simpan">
+</tr>
+
+
+</table
+
+
+
+
+
+</html>
